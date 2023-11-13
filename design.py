2d17f935855ae15970fec6eed04f199118564e37
Olie <ojimenez-gonzalez@unomaha.edu>
2023-11-12 19:50:57 -0600

    desing.py

--- design.py
+++ design.py
@@ -1,7 +1,9 @@
 import customtkinter as ctk
 
+# True for light mode
 appearence = True
 
+# func to change modes
 def  mode():
     global appearence
     if appearence:
@@ -17,16 +19,32 @@ def  mode():
 window = ctk.CTk()
 window.title("Calculator")
 window.geometry('400x700')
+window.resizable(False, False)
 
 # widgets
 opt_label = ctk.CTkLabel(window, text = 'a ctk label jit', 
                          fg_color=('white', 'black'), text_color= ('black', 'white'),
                          corner_radius= 10,)
-opt_label.pack()
+#opt_label.pack()
 
-button = ctk.CTkButton(window, text= 'a btton', fg_color='blue',
+button1 = ctk.CTkButton(window, text= 'a btton', fg_color='blue',
                         text_color='white', command = mode)
-button.pack()
+button1.grid(row=1, column =1)
+
+button2 = ctk.CTkButton(window, text= 'a btton 1', fg_color='blue',
+                        text_color='white')
+button2.grid(row=1, column =2)
+
+
+button3 = ctk.CTkButton(window, text= 'a btton', fg_color='blue',
+                        text_color='white')
+button3.grid(row=1, column =3)
+
+
+button4 = ctk.CTkButton(window, text= 'a btton', fg_color='blue',
+                        text_color='white')
+button4.grid(row=1, column =4)
+
 
 # run
 window.mainloop()
